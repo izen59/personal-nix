9d33ef98923dc5a67ca9c766f421a36eb59f6b7b
izen67 <17254913+izen67@users.noreply.github.com>
2026-01-04 14:49:16 +0200

    Update theme.nix

--- common/home-manager/hyprland/theme.nix
+++ common/home-manager/hyprland/theme.nix
@@ -4,10 +4,8 @@
   gtk = {
     enable = true;
 
-    # GTK4 / libadwaita: dark mode ONLY
     colorScheme = "dark";
 
-    # GTK3 only
     theme = {
       name = "Colloid";
       package = pkgs.colloid-gtk-theme;
@@ -24,19 +22,19 @@
       package = pkgs.bibata-cursors;
     };
   };
-  
+
   qt = {
     enable = true;
-  
+
     platformTheme.name = "gtk";
     style.name = "adwaita-dark";
-  
+
     kde.settings = {
       kdeglobals = {
         General = {
           ColorScheme = "BreezeDark";
         };
-  
+
         Icons = {
           Theme = "Papirus-Dark";
         };
@@ -44,15 +42,12 @@
     };
   };
 
-  # dconf is used only for settings not fully abstracted by HM
-  # and for non-GTK consumers (portals, Electron, Qt integration)
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
   };
 
-  # Cursor for non-GTK consumers (XWayland, misc toolkits)
   home.pointerCursor = {
     name = "Bibata-Modern-Classic";
     size = 24;
