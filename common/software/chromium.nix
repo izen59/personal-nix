65c95d33100c718864fa06d205156e018df6a3ca
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-16 19:09:04 +0200

    Update chromium.nix

--- common/software/chromium.nix
+++ common/software/chromium.nix
@@ -22,74 +22,19 @@
 
     # Extra sentence case flags
     extraOpts = {
-      # Telemetry off (0/false)
+      # Remaining essential policies (errors fixed, telemetry removed)
       MetricsReportingEnabled = false;
-      DeviceMetricsReportingEnabled = false;
-      HeartbeatEnabled = false;
-      DeviceActivityHeartbeatEnabled = false;
-      LogUploadEnabled = false;
-      DeviceFlexHwDataForProductImprovementEnabled = false;
-      DeviceExtensionsSystemLogEnabled = false;
-      EnableDeviceGranularReporting = false;
-      DeviceReportNetworkEvents = false;
-      DeviceReportRuntimeCounters = false;
-      DeviceReportXDREvents = false;
-      ReportAppInventory = false;
-      ReportAppUsage = false;
-      ReportWebsiteTelemetry = false;
-      ReportWebsiteActivityAllowlist = [ ];
-      ReportUploadFrequency = 0;
-      ReportArcStatusEnabled = false;
-      ReportCRDSessions = false;
-      ReportDeviceActivityTimes = false;
-      ReportDeviceAppInfo = false;
-      ReportDeviceAudioStatus = false;
-      ReportDeviceBacklightInfo = false;
-      ReportDeviceBluetoothInfo = false;
-      ReportDeviceBoardStatus = false;
-      ReportDeviceBootMode = false;
-      ReportDeviceCpuInfo = false;
-      ReportDeviceCrashReportInfo = false;
-      ReportDeviceFanInfo = false;
-      ReportDeviceGraphicsStatus = false;
-      ReportDeviceHardwareStatus = false;
-      ReportDeviceLoginLogout = false;
-      ReportDeviceMemoryInfo = false;
-      ReportDeviceNetworkConfiguration = false;
-      ReportDeviceNetworkInterfaces = false;
-      ReportDeviceNetworkStatus = false;
-      ReportDeviceOsUpdateStatus = false;
-      ReportDevicePeripherals = false;
-      ReportDevicePowerStatus = false;
-      ReportDevicePrintJobs = false;
-      ReportDeviceSecurityStatus = false;
-      ReportDeviceSessionStatus = false;
-      ReportDeviceStorageStatus = false;
-      ReportDeviceSystemInfo = false;
-      ReportDeviceTimezoneInfo = false;
-      ReportDeviceUsers = false;
-      ReportDeviceVersionInfo = false;
-      ReportDeviceVpdInfo = false;
-      CloudReportingEnabled = false;
-      CloudProfileReportingEnabled = false;
-      CloudExtensionRequestEnabled = false;
-      UserSecurityAuthenticatedReporting = false;
-      UserSecuritySignalsReporting = false;
-
-      # Privacy/Security (fixed types)
+    
       PasswordManagerEnabled = false;
       AutofillAddressEnabled = false;
       AutofillCreditCardEnabled = false;
-      BrowserSignin = 0;  # 0=disable
+      BrowserSignin = 0;
       SyncDisabled = true;
       BuiltInDnsClientEnabled = false;
       SafeBrowsingProtectionLevel = 1;
-      CloudPrintSubmitEnabled = false;
-      VariationReportingEnabled = false;
       SpellcheckEnabled = true;
       SpellcheckLanguage = [ "en-US" "ro-RO" ];
       DownloadRestrictions = 1;
-      SpellcheckServiceEnabled = false;
       WebRtcLocalIpsAllowedUrls = [ ];
       DnsOverHttpsMode = "secure";
     };
