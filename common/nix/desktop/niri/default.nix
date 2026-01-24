4c183a78447c6a562d220a651d5295b7d2480f4a
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-24 18:48:49 +0200

    Update default.nix

--- common/nix/desktop/niri/default.nix
+++ common/nix/desktop/niri/default.nix
@@ -9,6 +9,6 @@
 
 # Attach Niri Home Manager configs only when this desktop is active
   home-manager.users.${user}.imports = [
-    ../../home-manager/niri/default.nix
+    ../../../home-manager/niri/default.nix
   ];
 }
