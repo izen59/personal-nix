89b75f499152f1d28ea537527ba7c56cb78794b2
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-24 18:47:43 +0200

    Update default.nix

--- common/nix/desktop/hyprland/default.nix
+++ common/nix/desktop/hyprland/default.nix
@@ -9,6 +9,6 @@
 # Attach hyprland Home Manager configs only when this desktop is active
   home-manager.users.${user}.imports = [
     # Main hm hyprland import
-    ../../home-manager/hyprland/default.nix
+    ../../../home-manager/hyprland/default.nix
   ];
 }
