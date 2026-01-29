d9669c2168502e4ddb4dededc21f88be932845e8
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-29 23:10:53 +0200

    Update noctalia.nix

--- common/home-manager/hyprland/noctalia/noctalia.nix
+++ common/home-manager/hyprland/noctalia/noctalia.nix
@@ -1,12 +1,6 @@
 { config, lib, ... }:
 
 {
-  wayland.windowManager.hyprland.settings.bind= [
-    "SUPER, R, exec, noctalia-shell ipc call launcher toggle"
-    "SUPER, L, exec, noctalia-shell ipc call lockScreen lock"
-    "SUPER, V, exec, noctalia-shell ipc call launcher clipboard"
-  ];
-
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
