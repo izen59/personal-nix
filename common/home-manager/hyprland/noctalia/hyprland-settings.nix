d58013c273c2c23286e6003e22327274678e11ff
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-29 23:08:47 +0200

    Update hyprland-settings.nix

--- common/home-manager/hyprland/noctalia/hyprland-settings.nix
+++ common/home-manager/hyprland/noctalia/hyprland-settings.nix
@@ -3,7 +3,7 @@
 {
     wayland.windowManager.hyprland.settings = {
         bind = [
-          "SUPER, R, exec, sh -c 'noctalia-shell ipc call launcher toggle'"
+          "SUPER, R, exec, sh -c 'noctalia-shell ipc call launcher toggle > /tmp/noctalia-ipc.out 2> /tmp/noctalia-ipc.err; echo $? > /tmp/noctalia-ipc.code'"
           "SUPER, L, exec, sh -c 'noctalia-shell ipc call lockScreen lock'"
           "SUPER, V, exec, sh -c 'noctalia-shell ipc call launcher clipboard'"
         ];
