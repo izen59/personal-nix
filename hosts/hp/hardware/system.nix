3b509690cd8a28a9933651f681d8960c452a91bd
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-29 21:27:40 +0200

    Update system.nix

--- hosts/hp/hardware/system.nix
+++ hosts/hp/hardware/system.nix
@@ -7,4 +7,10 @@
   boot.loader.grub.useOSProber = true;
   # Use LTS kernel.
   boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  # Laptop Stuff
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
+  powerManagement.enable = true;
+  services.thermald.enable = true;
 }
