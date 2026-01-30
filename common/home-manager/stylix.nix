2ae1f585cfcc3d0f541cf1891324e4c9fc22bf23
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-30 18:11:35 +0200

    Create stylix.nix

---
+++ common/home-manager/stylix.nix
@@ -0,0 +1,74 @@
+{ inputs, pkgs, ... }:
+
+{
+  imports = [
+    inputs.stylix.homeManagerModules.stylix
+  ];
+
+  stylix = {
+    enable = true;
+
+    # Enable most targets automatically
+    autoEnable = true;
+
+    # Force dark mode
+    polarity = "dark";
+
+    # Color palette
+    base16Scheme =
+      "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    # Icons
+    icons = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+      dark = "Papirus-Dark";
+      light = "Papirus";
+    };
+
+    # Cursor
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+
+    # Fonts
+    fonts = {
+      serif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes = {
+        applications = 11;
+        desktop = 11;
+        terminal = 12;
+        popups = 11;
+      };
+    };
+
+    # Subtle terminal transparency
+    opacity = {
+      terminal = 0.95;
+      applications = 1.0;
+      desktop = 1.0;
+      popups = 1.0;
+    };
+  };
+}
