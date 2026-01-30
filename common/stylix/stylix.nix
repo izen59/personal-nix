151d7788264a314958bf6767685c6ac88cba7955
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-30 16:56:41 +0200

    Update stylix.nix

--- common/stylix/stylix.nix
+++ common/stylix/stylix.nix
@@ -1,13 +1,17 @@
 { inputs, config, pkgs, ... }:
 
 {
+  imports = [
+    inputs.stylix.nixosModules.stylix
+  ];
+
   stylix = {
     enable = true;
 
     # Global dark preference
     polarity = "dark";
 
-    # Color palette (no wallpaper required)
+    # Color palette
     base16Scheme =
       "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
