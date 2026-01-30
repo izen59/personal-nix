30a72aa15f4443a17123f15b177abe8ee5042b89
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-30 17:55:45 +0200

    Create kitty.nix

---
+++ common/home-manager/stylix/kitty.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  stylix.targets.kitty = {
+    enable = true;
+
+    colors.enable = true;
+    fonts.enable = true;
+    inputs.enable = true;
+    opacity.enable = true;
+
+    # Optional: richer palette mode
+    variant256Colors = false;
+  };
+}
