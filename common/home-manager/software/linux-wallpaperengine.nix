222bf0aa1b1c5a342eb2920f2ab80473c5e32876
izen59 <17254913+izen59@users.noreply.github.com>
2026-01-30 22:31:42 +0200

    Update linux-wallpaperengine.nix

--- common/home-manager/software/linux-wallpaperengine.nix
+++ common/home-manager/software/linux-wallpaperengine.nix
@@ -5,14 +5,14 @@
     enable = true;
 
     assetsPath =
-      "/mnt/extras/SteamLibrary/steamapps/common/wallpaper_engine/assets";
+      "/media/extras/SteamLibrary/steamapps/common/wallpaper_engine/assets";
 
     wallpapers = [
       {
         monitor = "HDMI-A-1";
 
         wallpaperId =
-          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/2359043440";
+          "/media/extras/SteamLibrary/steamapps/workshop/content/431960/2359043440";
 
         fps = 30;
 
@@ -27,7 +27,7 @@
         monitor = "DP-1";
 
         wallpaperId =
-          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/2359043440";
+          "/media/extras/SteamLibrary/steamapps/workshop/content/431960/2359043440";
       }
     ];
   };
