18b3d22c18717da0ac1de52e89742b57e5d831e0
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-12 00:43:38 +0200

    Update system.nix

--- hosts/pc/nix/hardware/system.nix
+++ hosts/pc/nix/hardware/system.nix
@@ -8,6 +8,10 @@
   # Switch to official latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # Disable TPM (not working in dell)
+  security.tpm2.enable = false;
+  services.tpm2-abrmd.enable = false;
+
   # Limits to prevent system crashes and ease up on the CPU while building
   nix.settings = {
     max-jobs = 1;
