7c5f881bcfc14e836b6b1c855179ec58c7daca23
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-14 20:48:09 +0200

    Update gnome.nix

--- common/nix/desktop/gnome/gnome.nix
+++ common/nix/desktop/gnome/gnome.nix
@@ -14,36 +14,11 @@
     enable = true;
 
     defaultApplications = {
-      "inode/directory" = [ "nemo.desktop" ];
-      "x-directory/normal" = [ "nemo.desktop" ];
     };
   };
 
   # ────────────────────────────── System Packages ──────────────────────────────
   environment.systemPackages = with pkgs; [
-    ## Core tools you like
-    axel
-    bc
-    ripgrep
-    libqalculate
-    libnotify
-    playerctl
-    brightnessctl
-    ddcutil
-    wireplumber
-    networkmanager
-    easyeffects
-
-    ## File management / GUI tools
-    nemo-with-extensions
-    gnome-disk-utility
-    gnome-system-monitor
-
-    ## Theming
-    adw-gtk3
-    bibata-cursors
-    papirus-icon-theme
-
     ## GNOME Stuff
     gnome-tweaks
     gnome-browser-connector
@@ -53,23 +28,6 @@
     gnomeExtensions.blur-my-shell
     gnomeExtensions.just-perfection
     gnomeExtensions.vitals
-  ];
-
-  # ────────────────────────────── Fonts ──────────────────────────────
-  fonts.fontconfig.enable = true;
-  fonts.enableDefaultPackages = true;
-
-  fonts.packages = with pkgs; [
-    material-symbols
-    rubik
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.ubuntu
-    nerd-fonts.ubuntu-mono
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.fantasque-sans-mono
-    nerd-fonts.mononoki
-    nerd-fonts.space-mono
-    maple-mono.Normal-NF
   ];
 
   # ────────────────────────────── File Management Services ──────────────────────────────
