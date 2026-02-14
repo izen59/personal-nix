30402d43095dfc53e19031ede51fd0137acda11c
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-14 13:49:14 +0200

    Update system.nix

--- hosts/toshiba/nix/hardware/system.nix
+++ hosts/toshiba/nix/hardware/system.nix
@@ -6,7 +6,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
   # Use LTS kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   # Laptop Stuff
   services.upower.enable = true;
