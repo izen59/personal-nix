ed6dd9ffbc36c0ab4412a75893fb8c030c16c333
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-14 14:16:10 +0200

    Update system.nix

--- common/nix/hardware/system.nix
+++ common/nix/hardware/system.nix
@@ -1,9 +1,7 @@
 { config, pkgs, inputs, user, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.download-buffer-size = 262144000;
 
   # Important: defines compatibility with a specific NixOS release.
   # Do not change casually; bump only when upgrading across releases.
