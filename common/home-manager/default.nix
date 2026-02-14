440f0460c64e83138f793b45b7d9b4c99f613d11
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-14 13:32:53 +0200

    Update default.nix

--- common/home-manager/default.nix
+++ common/home-manager/default.nix
@@ -1,11 +1,6 @@
 { inputs, user, ... }:
 
 {
-  # Import the Home Manager NixOS module
-  imports = [
-    inputs.home-manager.nixosModules.default
-  ];
-
   home-manager = {
     # Let Home Manager see flake inputs and user
     extraSpecialArgs = { inherit inputs user; };
