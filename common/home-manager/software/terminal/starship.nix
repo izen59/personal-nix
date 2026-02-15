f9cdb14e8055ac4c3cc4b13869464173124a536f
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-15 13:07:29 +0200

    Update starship.nix

--- common/home-manager/software/terminal/starship.nix
+++ common/home-manager/software/terminal/starship.nix
@@ -7,21 +7,21 @@
     settings = {
       add_newline = false;
 
-      format = "$time$cmd_duration$username[](fg:accent bg:surface2)$directory[](fg:surface2 bg:surface3)$git_branch$git_status[](fg:surface3)$character";
+      format = "$time$cmd_duration$username[](fg:accent bg:surface2)$directory[](fg:surface2 bg:surface3)$git_branch$git_status[](fg:surface3)$character";
 
       time = {
         disabled = false;
         time_format = "%R";
-        format = "[ $time ](fg:accent bg:surface)";
+        format = "[ 󱑍 $time ](fg:accent bg:surface)";
       };
 
       cmd_duration = {
         min_time = 3000;
-        format = "[ $duration ](fg:accent bg:surface)";
+        format = "[󱑎 $duration ](fg:accent bg:surface)";
       };
 
       username = {
-        format = "[ $user ](fg:accent bg:surface)";
+        format = "[$user ](fg:accent bg:surface)";
       };
 
       directory = {
@@ -31,16 +31,16 @@
       };
 
       git_branch = {
-        format = "[ $branch(:$remote_branch) ](fg:text bg:surface3)";
+        format = "[   $branch(:$remote_branch) ](fg:text bg:surface3)";
       };
 
       git_status = {
-        format = "[ $all_status ](fg:accent_soft bg:surface3)";
+        format = "[$all_status ](fg:accent_soft bg:surface3)";
       };
 
       character = {
-        success_symbol = "[>](fg:accent)";
-        error_symbol   = "[X](fg:warn)";
+        success_symbol = "[❯](fg:accent)";
+        error_symbol   = "[✗](fg:warn)";
       };
     };
   };
