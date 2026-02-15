14e395cb071ce85fcaf9b6440a196bfa60763c63
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-15 13:10:46 +0200

    Update gdm.nix

--- common/nix/desktop/gnome/login/gdm.nix
+++ common/nix/desktop/gnome/login/gdm.nix
@@ -10,4 +10,17 @@
     fish
     bashInteractive
   ];
+
+  # Sets monitor config to be as same as desktop
+  systemd.services.gdm-monitors = {
+    description = "Sync GNOME monitors.xml to GDM";
+    wantedBy = [ "display-manager.service" ];
+    before = [ "display-manager.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ConditionPathExists = "/home/${user}/.config/monitors.xml";
+      ExecStart = "${pkgs.coreutils}/bin/cp -f /home/${user}/.config/monitors.xml /etc/xdg/monitors.xml";
+      ExecStartPost = "${pkgs.coreutils}/bin/chmod 0644 /etc/xdg/monitors.xml";
+    };
 }
