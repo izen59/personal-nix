a42ae0c6adf88985f4884acbafcc07fa7874cd72
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-17 14:36:12 +0200

    Update sddm.nix

--- common/nix/desktop/kde/login/sddm.nix
+++ common/nix/desktop/kde/login/sddm.nix
@@ -9,4 +9,33 @@
     autoNumlock = true;
     #enableHidpi = true;
   };
+
+  # Sets monitor config to be the same as desktop
+  systemd.services.sddm-plasma-config = {
+    description = "Sync Plasma display/input config to SDDM";
+    wantedBy = [ "display-manager.service" ];
+    before = [ "display-manager.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+
+      ExecStart = "${pkgs.bash}/bin/bash -c ''
+        set -e
+
+        mkdir -p /var/lib/sddm/.config
+
+        if [ -f /home/${user}/.config/kwinoutputconfig.json ]; then
+          ${pkgs.coreutils}/bin/cp -f /home/${user}/.config/kwinoutputconfig.json /var/lib/sddm/.config/
+          ${pkgs.coreutils}/bin/chown sddm:sddm /var/lib/sddm/.config/kwinoutputconfig.json
+          ${pkgs.coreutils}/bin/chmod 0644 /var/lib/sddm/.config/kwinoutputconfig.json
+        fi
+
+        if [ -f /home/${user}/.config/kcminputrc ]; then
+          ${pkgs.coreutils}/bin/cp -f /home/${user}/.config/kcminputrc /var/lib/sddm/.config/
+          ${pkgs.coreutils}/bin/chown sddm:sddm /var/lib/sddm/.config/kcminputrc
+          ${pkgs.coreutils}/bin/chmod 0644 /var/lib/sddm/.config/kcminputrc
+        fi
+      ''";
+    };
+  };
 }
