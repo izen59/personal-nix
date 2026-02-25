476afcedf024410167aef417752fb19a7dc29c53
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-25 15:48:21 +0200

    Update network-mounts.nix

--- common/nix/hardware/network-mounts.nix
+++ common/nix/hardware/network-mounts.nix
@@ -1,15 +1,5 @@
 { config, ... }:
 
 {
-  fileSystems."/mnt/samba-zoneminder" = {
-    device = "//192.168.50.102/zoneminder";
-    fsType = "cifs";
-    options = [
-      "_netdev"
-      "noatime"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-    ];
-  };
 
 }
