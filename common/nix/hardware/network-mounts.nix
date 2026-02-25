4096ca4b8317d71cbd436ef6fd54e1bff3cca99b
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-25 15:47:01 +0200

    Update network-mounts.nix

--- common/nix/hardware/network-mounts.nix
+++ common/nix/hardware/network-mounts.nix
@@ -12,37 +12,4 @@
     ];
   };
 
-fileSystems."/mnt/samba-media" = {
-  device = "//192.168.50.102/MEDIA";
-  fsType = "cifs";
-  options = [
-    "_netdev"
-    "noatime"
-    "x-systemd.automount"
-    "x-systemd.idle-timeout=60"
-  ];
-};
-
-fileSystems."/mnt/samba-storage" = {
-  device = "//192.168.50.102/STORAGE";
-  fsType = "cifs";
-  options = [
-    "_netdev"
-    "noatime"
-    "x-systemd.automount"
-    "x-systemd.idle-timeout=60"
-  ];
-};
-
-fileSystems."/mnt/samba-torrent" = {
-  device = "//192.168.50.102/TORRENT";
-  fsType = "cifs";
-  options = [
-    "_netdev"
-    "noatime"
-    "x-systemd.automount"
-    "x-systemd.idle-timeout=60"
-  ];
-};
-
 }
