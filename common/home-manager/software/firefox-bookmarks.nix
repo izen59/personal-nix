0690e76fd229100241b2c94f768ac07156c4240f
izen59 <17254913+izen59@users.noreply.github.com>
2026-02-28 01:11:11 +0200

    Update firefox-bookmarks.nix

--- common/home-manager/software/firefox-bookmarks.nix
+++ common/home-manager/software/firefox-bookmarks.nix
@@ -18,7 +18,6 @@
             bookmarks = [
               { name = "Proxmox"; url = "http://homelab:8006/"; }
               { name = "ZM"; url = "http://zoneminder:8095/"; }
-              { name = "AdGuard"; url = "http://adguard:3000/"; }
               { name = "Traefik Stats"; url = "http://traefik:7890/"; }
               { name = "OMV"; url = "http://192.168.50.101/"; }
               { name = "qBittorrent"; url = "http://arr:8080/"; }
